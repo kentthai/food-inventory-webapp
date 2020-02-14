3372c24217452ea45578ff59750d5d5c34a35065
Kent Thai <ket037@ucsd.edu>
2020-02-14 10:25:07 -0800

    Fixed links

--- routes/claim.js
+++ routes/claim.js
@@ -12,25 +12,9 @@ exports.foodInfo = function(request, response) {
   console.log("group: ");
   console.log(group["foodItems"]);
 
-  // Generate id
-	var set = [];
-	var i;
-	for (i = 0; i < group.foodItems.length; i += 1) {
-		if (group.foodItems[i] != null) {
-			set.push(parseInt(group.foodItems[i]["id"]));
-		}
-	}
-	console.log(set);
+  var latestId = foods["foodItems"].length+1;
 
-	var id = 1;
-	while (set.indexOf(id) >= 0) {
-		id += 1;
-	}
-
-  //var latestId = foods["foodItems"].length+1;
-
-  var groupFood = group["foodItems"][id]; // of by one, our first project has index 0
-
+  var groupFood = group["foodItems"][groupFoodID-1]; // of by one, our first project has index 0
   groupFood["id"] = latestId.toString();
 
   foods.foodItems.push(groupFood);
