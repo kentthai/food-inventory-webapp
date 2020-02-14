6e3d53134cd6cb294b0dd494a97709124d150562
Kent Thai <ket037@ucsd.edu>
2020-02-13 22:43:08 -0800

    Fixed links

--- routes/add.js
+++ routes/add.js
@@ -13,7 +13,9 @@ exports.addPersonal = function(request, response) {
 	// Generate id
 	var set = new Set();
 	for (let i = 0; i < foods.foodItems.length; i += 1) {
-		set.add(parseInt(foods.foodItems[i]["id"]));
+		if (foods.foodItems[i] != null) {
+			set.add(parseInt(foods.foodItems[i]["id"]));
+		}
 	}
 	console.log(set);
 
@@ -52,7 +54,9 @@ exports.addGroup = function(request, response) {
 	// Generate id
 	var set = new Set();
 	for (let i = 0; i < group.foodItems.length; i += 1) {
-		set.add(parseInt(group.foodItems[i]["id"]));
+		if (foods.foodItems[i] != null) {
+			set.add(parseInt(group.foodItems[i]["id"]));
+		}
 	}
 	console.log(set);
 
