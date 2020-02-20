bec970596059c01dd8657e9a6d906f4831b13e3c
Rakesh Senthilvelan <rsenthil@ucsd.edu>
2020-02-19 17:04:40 -0800

    Added movement from group page to addForm

--- public/js/group.js
+++ public/js/group.js
@@ -118,20 +118,20 @@ container.addEventListener('click', function(event) {
 */
 
 // Reveals add form
-add.addEventListener('click', function(event){
-	event.preventDefault();
-	var addBox = document.querySelector("#addForm");
-	addBox.classList.remove("hidden");
-
-}, false);
-
-
-// Close form on cancel
-cancel.addEventListener('click', function(event){
-	event.preventDefault();
-	var addBox = document.querySelector("#addForm");
-	addBox.classList.add("hidden");
-},false);
+// add.addEventListener('click', function(event){
+// 	event.preventDefault();
+// 	var addBox = document.querySelector("#addForm");
+// 	addBox.classList.remove("hidden");
+//
+// }, false);
+//
+//
+// // Close form on cancel
+// cancel.addEventListener('click', function(event){
+// 	event.preventDefault();
+// 	var addBox = document.querySelector("#addForm");
+// 	addBox.classList.add("hidden");
+// },false);
 
 // Close form on OK
 ok.addEventListener('click', function(event){
